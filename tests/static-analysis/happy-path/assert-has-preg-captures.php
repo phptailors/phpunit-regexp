a80c238fdd3e29005476e8aa70f965eb89b794b8
ptomulik <pawel@tomulik.pl>
2026-04-11 15:31:18 +0000

    remove apcu_* functions from composer-require-checkers whitelist

--- tests/static-analysis/happy-path/assert-has-preg-captures.php
+++ tests/static-analysis/happy-path/assert-has-preg-captures.php
@@ -14,7 +14,7 @@ use PHPUnit\Framework\ExpectationFailedException;
 use SebastianBergmann\RecursionContext\InvalidArgumentException;
 use Tailors\PHPUnit\HasPregCapturesTrait;
 
-class Assert extends \PHPUnit\Framework\Assert
+final class Assert extends \PHPUnit\Framework\Assert
 {
     use HasPregCapturesTrait;
 }
