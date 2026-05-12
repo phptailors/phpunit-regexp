5802e83f0269f97042517507de740ef1efc30fd8
ptomulik <pawel@tomulik.pl>
2026-05-12 09:52:10 +0000

    Merge branch '2.0' into 3.0

--- src/Constraint/HasPregCaptures.php
+++ src/Constraint/HasPregCaptures.php
@@ -84,11 +84,9 @@ final class HasPregCaptures extends Constraint
      * a boolean value instead: true in case of success, false in case of a
      * failure.
      *
-     * @param mixed $other
-     *
      * @throws ExpectationFailedException
      */
-    public function evaluate($other, string $description = '', bool $returnResult = false): ?bool
+    public function evaluate(mixed $other, string $description = '', bool $returnResult = false): ?bool
     {
         $success = $this->matches($other);
 
@@ -198,8 +196,6 @@ final class HasPregCaptures extends Constraint
     }
 
     /**
-     * @return array[]
-     *
      * @psalm-return array{0: array, 1: array}
      */
     private function getArraysForComparison(array $matches): array
