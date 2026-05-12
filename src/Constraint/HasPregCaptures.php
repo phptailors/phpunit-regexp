0fd53d32e34f87fb71d3958c00eb608b7f3c6f00
ptomulik <pawel@tomulik.pl>
2026-05-12 06:55:19 +0000

    make psalm happy on 1.0

--- src/Constraint/HasPregCaptures.php
+++ src/Constraint/HasPregCaptures.php
@@ -92,9 +92,7 @@ final class HasPregCaptures extends Constraint
      * a boolean value instead: true in case of success, false in case of a
      * failure.
      *
-     * @param mixed  $other
-     * @param string $description
-     * @param bool   $returnResult
+     * @param mixed $other
      *
      * @throws ExpectationFailedException
      * @throws \SebastianBergmann\RecursionContext\InvalidArgumentException
--- src/HasPregCapturesTrait.php
+++ src/HasPregCapturesTrait.php
@@ -21,9 +21,7 @@ trait HasPregCapturesTrait
     /**
      * Evaluates a \PHPUnit\Framework\Constraint\Constraint matcher object.
      *
-     * @param mixed      $value
-     * @param Constraint $constraint
-     * @param string     $message
+     * @param mixed $value
      *
      * @throws ExpectationFailedException
      * @throws InvalidArgumentException
