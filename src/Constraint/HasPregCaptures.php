c9c6923f4042c6086668be0d8bbccc1e7d4f2431
ptomulik <pawel@tomulik.pl>
2026-05-12 09:38:26 +0000

    cleaned up @return docblocks on 1.0

--- src/Constraint/HasPregCaptures.php
+++ src/Constraint/HasPregCaptures.php
@@ -209,8 +209,6 @@ final class HasPregCaptures extends Constraint
     }
 
     /**
-     * @return array[]
-     *
      * @psalm-return array{0: array, 1: array}
      */
     private function getArraysForComparison(array $matches): array
